770524b71aac8ae3c6b6c92fc11ce27650590938
meerhamzadev <hamzababar37@gmail.com>
2022-02-19 00:09:58 +0500

    🐛Fix: new page opening issue

--- client-side/src/Components/SideBar/SideBar.js
+++ client-side/src/Components/SideBar/SideBar.js
@@ -1,46 +1,52 @@
-import {AccountCircle, GpsFixed, HowToReg, LocalHospital, MenuOpen, Settings} from '@material-ui/icons';
-import {ContactMail, Info, Policy} from '@mui/icons-material';
+import { AccountCircle, GpsFixed, HowToReg, LocalHospital, MenuOpen, Settings } from '@material-ui/icons';
+import { ContactMail, Info, Policy } from '@mui/icons-material';
 import Logout from '@mui/icons-material/Logout';
 import PersonSearch from '@mui/icons-material/PersonSearch';
-import {Box, Divider, Drawer, IconButton, List, ListItem, ListItemIcon, ListItemText} from '@mui/material';
-import React, {useState} from 'react';
-import {auth} from '../../Firebase';
+import { Box, Divider, Drawer, IconButton, List, ListItem, ListItemIcon, ListItemText } from '@mui/material';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { auth } from '../../Firebase';
 import "./SideBar.css";
 
-function SideBar({profile}) {
+function SideBar({ profile }) {
     const [sideBarState, setSideBarState] = useState(false);
 
     const toggleDrawer = (anchor, open) => (event) => {
         if (event.type === 'keydown' && (event.key === 'Tab' || event.key === 'Shift')) {
             return;
         }
-
         setSideBarState(open);
     };
-    
+
     const list = (anchor) => (
         <Box sx={{ width: 300 }} role="presentation" onClick={toggleDrawer(anchor, false)} onKeyDown={toggleDrawer(anchor, false)}>
             {
                 (profile) ? (
                     <List>
-                        <ListItem button onClick={() => window.open("/", "__self")}>
-                            <ListItemIcon>
-                                <AccountCircle />
-                            </ListItemIcon>
-                            <ListItemText primary={profile} />
-                        </ListItem>
-                        <ListItem button onClick={() => window.open("/settings", "__self")}>
-                            <ListItemIcon>
-                                <Settings />
-                            </ListItemIcon>
-                            <ListItemText primary={"Settings"} />
-                        </ListItem>
-                        <ListItem button onClick={() => window.open("/finddonor", "__self")}>
-                            <ListItemIcon>
-                                <PersonSearch />
-                            </ListItemIcon>
-                            <ListItemText primary={"Find Donor"} />
-                        </ListItem>
+                        <Link to="/" >
+                            <ListItem>
+                                <ListItemIcon>
+                                    <AccountCircle />
+                                </ListItemIcon>
+                                <ListItemText primary={profile} />
+                            </ListItem>
+                        </Link>
+                        <Link to='/settings' className='settings' >
+                            <ListItem>
+                                <ListItemIcon>
+                                    <Settings />
+                                </ListItemIcon>
+                                <ListItemText primary={"Settings"} />
+                            </ListItem>
+                        </Link>
+                        <Link to="/finddonor" >
+                            <ListItem>
+                                <ListItemIcon>
+                                    <PersonSearch />
+                                </ListItemIcon>
+                                <ListItemText primary={"Find Donor"} />
+                            </ListItem>
+                        </Link>
                         <ListItem button onClick={() => auth.signOut()}>
                             <ListItemIcon>
                                 <Logout />
@@ -50,30 +56,38 @@ function SideBar({profile}) {
                     </List>
                 ) : (
                     <List>
-                        <ListItem button onClick={() => window.open('/login', "__self")}>
-                            <ListItemIcon>
-                                <AccountCircle />
-                            </ListItemIcon>
-                            <ListItemText primary={"Login"} />
-                        </ListItem>
-                        <ListItem button onClick={() => window.open('/register', "__self")}>
-                            <ListItemIcon>
-                                <HowToReg />
-                            </ListItemIcon>
-                            <ListItemText primary={"Register"} />
-                        </ListItem>
-                        <ListItem button onClick={() => window.open("/locationservice", "__self")}>
-                            <ListItemIcon>
-                                <GpsFixed />
-                            </ListItemIcon>
-                            <ListItemText primary={"Donors Around You"} />
-                        </ListItem>
-                        <ListItem button onClick={() => window.open("/locationservice", "__self")}>
-                            <ListItemIcon>
-                                <LocalHospital />
-                            </ListItemIcon>
-                            <ListItemText primary={"Hospitals Around You"} />
-                        </ListItem>
+                        <Link to="/login" >
+                            <ListItem>
+                                <ListItemIcon>
+                                    <AccountCircle />
+                                </ListItemIcon>
+                                <ListItemText primary={"Login"} />
+                            </ListItem>
+                        </Link>
+                        <Link to="/register" >
+                            <ListItem>
+                                <ListItemIcon>
+                                    <HowToReg />
+                                </ListItemIcon>
+                                <ListItemText primary={"Register"} />
+                            </ListItem>
+                        </Link>
+                        <Link to="/locationservice" >
+                            <ListItem >
+                                <ListItemIcon>
+                                    <GpsFixed />
+                                </ListItemIcon>
+                                <ListItemText primary={"Donors Around You"} />
+                            </ListItem>
+                        </Link>
+                        <Link to="/locationservice" >
+                            <ListItem >
+                                <ListItemIcon>
+                                    <LocalHospital />
+                                </ListItemIcon>
+                                <ListItemText primary={"Hospitals Around You"} />
+                            </ListItem>
+                        </Link>
                     </List>
                 )
             }
@@ -100,13 +114,13 @@ function SideBar({profile}) {
             </List>
         </Box>
     );
-    
+
     return (
         <div className="sidebar">
             {
                 <React.Fragment key={"left"}>
-                    <IconButton children={<MenuOpen style={{fontSize: '3vw'}} />} color="primary" onClick={toggleDrawer("left", true)} />
-                    <Drawer anchor={"left"} open={sideBarState} onClose={toggleDrawer("left", false)}>
+                    <IconButton children={<MenuOpen style={{ fontSize: '3vw' }} />} color="error" onClick={toggleDrawer("left", true)} />
+                    <Drawer anchor={"left"} open={sideBarState} onClose={toggleDrawer("left", true)}>
                         {list("left")}
                     </Drawer>
                 </React.Fragment>
